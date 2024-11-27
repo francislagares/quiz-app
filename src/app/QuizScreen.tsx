43c39c59227931e8fc402e6b87639c76ff060e67
Francis Lagares <francis.lagares@gmail.com>
2024-11-27 20:27:31 +0100

    feat: project scaffolding

---
+++ src/app/QuizScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+const QuizScreen = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: '#FDFEF4',
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+      }}
+    >
+      <Text>QuizScreen</Text>
+    </View>
+  );
+};
+
+export default QuizScreen;
---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import QuizScreen from './QuizScreen';
+
+const Index = () => {
+  return <QuizScreen />;
+};
+
+export default Index;
